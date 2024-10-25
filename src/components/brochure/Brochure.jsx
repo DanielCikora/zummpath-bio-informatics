89fbe8f29ac8de04a24252088006387453adc118
DanielCikora <d.cikora98@gmail.com>
2024-10-25 23:27:25 +0200

    Cards - Now Open onClick anywhere. Plus Icon changes to Minus once Open.

--- src/components/brochure/Brochure.jsx
+++ src/components/brochure/Brochure.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const Brochure = () => {
   const brochureCardsTexts = [
     {
@@ -28,14 +29,14 @@ const Brochure = () => {
         "This internship serves as a launchpad for potential full-time roles, like Research Assistant, at Quantumzyme. Take your first step toward a fulfilling career in bioinformatics.",
     },
   ];
-  const [expandedCards, setExpandedCards] = useState(
-    new Array(brochureCardsTexts.length).fill(false)
-  );
+
+  const [expandedCardIndex, setExpandedCardIndex] = useState(null); // Store the index of the currently expanded card
+
   const toggleViewMore = (index) => {
-    const updatedExpandedCards = [...expandedCards];
-    updatedExpandedCards[index] = !updatedExpandedCards[index];
-    setExpandedCards(updatedExpandedCards);
+    // If the clicked card is already expanded, close it, otherwise open it and close any other card
+    setExpandedCardIndex((prevIndex) => (prevIndex === index ? null : index));
   };
+
   return (
     <section className='brochure pt-8 pb-10'>
       <div className='wrapper'>
@@ -46,32 +47,37 @@ const Brochure = () => {
           <div className='brochure-boxes flex md:flex-row flex-col gap-5 justify-between'>
             {brochureCardsTexts.map((brochureCardsText, index) => (
               <div
-                className='bg-lightGreen min-h-[500px] flex flex-col justify-between bg-opacity-60 md:text-left text-center px-6 py-10 rounded-xl text-offWhite w-full max-w-[380px]'
+                className='bg-lightGreen cursor-pointer min-h-[500px] flex flex-col justify-between bg-opacity-60 md:text-left text-center px-6 py-10 rounded-xl text-offWhite w-full max-w-[380px]'
                 key={brochureCardsText.id}
+                onClick={() => toggleViewMore(index)}
               >
                 <h3 className='text-royalGreen md:text-3xl text-2xl font-semibold'>
                   {brochureCardsText.title}
                 </h3>
                 <div className='overflow-hidden'>
                   <p
-                    className={`block text-black md:text-xl text-lg transition-all duration-500 ease-in-out ${
-                      expandedCards[index]
+                    className={`block text-black md:text-2xl text-xl transition-all duration-500 ease-in-out ${
+                      expandedCardIndex === index
                         ? "max-h-[400px] opacity-100" // Fully visible
                         : "max-h-[100px] opacity-100" // Limit height for collapsed state
                     }`}
                   >
-                    {expandedCards[index]
+                    {expandedCardIndex === index
                       ? brochureCardsText.paragraph
                       : `${brochureCardsText.paragraph.slice(0, 50)}...`}
                   </p>
                 </div>
-                <button
-                  className='block max-w-fit font-bold text-xl rounded-full bg-royalGreen px-2.5 py-2 mt-2'
-                  type='button'
-                  onClick={() => toggleViewMore(index)} // Pass the index of the clicked card
-                >
-                  <FontAwesomeIcon className='block text-2xl' icon={faPlus} />
-                </button>
+                {expandedCardIndex === index ? (
+                  <FontAwesomeIcon
+                    className='block text-2xl max-w-fit font-bold rounded-full bg-royalGreen px-2.5 py-2'
+                    icon={faMinus}
+                  />
+                ) : (
+                  <FontAwesomeIcon
+                    className='block text-2xl max-w-fit font-bold rounded-full bg-royalGreen px-2.5 py-2'
+                    icon={faPlus}
+                  />
+                )}
               </div>
             ))}
           </div>
