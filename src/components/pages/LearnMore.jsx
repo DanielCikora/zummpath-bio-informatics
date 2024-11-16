ff0a0a0485edbcdeda3347e2128d7929b7be056b
Daniel Cikora <d.cikora98@gmail.com>
2024-11-16 23:38:54 +0100

    Program - OnScroll Effect Edited. LearnMore - Rendered Data Changed, Styling Edited.

--- src/components/pages/LearnMore.jsx
+++ src/components/pages/LearnMore.jsx
@@ -2,115 +2,111 @@ import QuantumzymeImg from "../assets/images/learnmoreimages/quantumzyme.png";
 import ZummitImg from "../assets/images/learnmoreimages/Zummit.png";
 
 const LearnMore = () => {
+  const content = [
+    {
+      type: "text",
+      title: "Program Fee",
+      paragraphPrice: "₹ 59,999.99",
+      quantumImage: QuantumzymeImg,
+      quantumAlt: "Quantumzyme Logo",
+      zummitImage: ZummitImg,
+      zummitAlt: "Zummit Logo",
+    },
+    {
+      type: "text",
+      title: "Program Highlights",
+      paragraph:
+        "Jumpstart your bioinformatics career with a specialized training program that combines knowledge, hands-on experience, and direct industry exposure. Our partnership with Quantumzyme offers participants an in-depth look into molecular simulations, enzyme engineering, and computational biology, culminating in a guaranteed six-month Industrial Training Program that can propel your career forward.",
+    },
+    {
+      type: "list",
+      title: "Our Advantages",
+      listItems: [
+        "Expert-Guided Learning: Learn from industry leaders and bioinformatics specialists who bring real-world insights and cutting-edge techniques.",
+        "Project-Based Learning: Apply your knowledge to challenging projects that mirror industry needs, making you career-ready.",
+        "Exclusive Industrial Training Program Opportunity: Secure a six-month Industrial Training Program at Quantumzyme's lab in Bangalore, giving you unparalleled exposure to enzyme engineering and research projects.",
+        "Seamless Career Transition: Outstanding interns may transition into full-time Research Assistant roles at Quantumzyme, setting you up for a successful bioinformatics career.",
+      ],
+    },
+    {
+      type: "list",
+      title: "Curriculum Roadmap",
+      listItems: [
+        "Foundation of Insilico Techniques: Start with core bioinformatics principles and basic computational skills to establish a strong foundation.",
+        "Advanced Analysis Tools: Develop your analytical abilities with in-depth techniques in molecular simulations and enzyme studies.",
+        "Modeling for Real-World Applications: Explore complex modeling methods to tackle real-world bioinformatics challenges.",
+        "AI and Automation: Master AI and machine learning applications for automation, giving you a tech-driven edge in bioinformatics.",
+      ],
+    },
+  ];
+
   return (
-    <article className='learn-more'>
-      <div className='wrapper'>
-        <div className='learn-more__content py-16'>
-          <h1 className='text-5xl font-semibold text-royalGreen text-center'>
+    <section className='learn-more pt-20'>
+      <div className='min-h-dvh'>
+        <div className='wrapper'>
+          <h1 className='md:text-5xl text-4xl font-bold text-center text-royalGreen mb-20'>
             Learn More
           </h1>
-          <div className='flex items-center'>
-            <img
-              src={QuantumzymeImg}
-              alt='Quantumzyme-Logo-Image'
-              className='h-auto w-full max-w-40 block ml-auto'
-            />
-          </div>
-
-          <div className='flex items-center py-9'>
-            <h2 className='text-3xl font-semibold w-[200px] pr-5'>
-              Program
-              <br />
-              Fee
-            </h2>
-            <h2 className='text-6xl max-w-[1000px]'>₹ 59,999.99</h2>
-            <div className='flex flex-col gap-3 ml-auto'>
-              <p className='text-xl text-center'>in partenership</p>
-              <img
-                src={ZummitImg}
-                alt='Zummit-Logo-Image'
-                className='h-auto w-full max-w-40 block'
-              />
-            </div>
-          </div>
-          <hr className='h-2 w-full bg-royalGreen'></hr>
-          <div className='flex items-center py-9'>
-            <h2 className='text-3xl font-semibold w-[200px] pr-5'>
-              Program
-              <br />
-              Highlights
-            </h2>
-            <p className='text-xl max-w-[1000px]'>
-              Jumpstart your bioinformatics career with a specialized training
-              program that combines knowledge, hands-on experience, and direct
-              industry exposure. Our partnership with Quantumzyme offers
-              participants an in-depth look into molecular simulations, enzyme
-              engineering, and computational biology, culminating in a
-              guaranteed six-month Industrial Training Program that can propel
-              your career forward.
-            </p>
-          </div>
-          <hr className='h-0.5 w-full bg-gray-400'></hr>
-          <div className='flex items-center py-9'>
-            <h2 className='text-3xl font-semibold w-[200px] pr-5'>
-              Our
-              <br />
-              Advantages
-            </h2>
-            <ul className='list-disc text-xl max-w-[1000px] pl-5'>
-              <li>
-                Expert-Guided Learning: Learn from industry leaders and
-                bioinformatics specialists who bring real-world insights and
-                cutting-edge techniques.
-              </li>
-              <li>
-                Project-Based Learning: Apply your knowledge to challenging
-                projects that mirror industry needs, making you career-ready.
-              </li>
-              <li>
-                Exclusive Industrial Training Program Opportunity: Secure a
-                six-month Industrial Training Program at Quantumzyme's lab in
-                Bangalore, giving you unparalleled exposure to enzyme
-                engineering and research projects.
-              </li>
-              <li>
-                Seamless Career Transition: Outstanding interns may transition
-                into full-time Research Assistant roles at Quantumzyme, setting
-                you up for a successful bioinformatics career.
-              </li>
-            </ul>
-          </div>
-          <hr className='h-0.5 w-full bg-gray-400'></hr>
-          <div className='flex items-center py-9'>
-            <h2 className='text-3xl font-semibold w-[200px] pr-5'>
-              Curriculum
-              <br />
-              Roadmap
-            </h2>
-            <ul className='list-disc text-xl max-w-[1000px] pl-5'>
-              <li>
-                Foundation of In silico Techniques Start with core
-                bioinformatics principles and basic computational skills to
-                establish a strong foundation.
-              </li>
-              <li>
-                Advanced Analysis Tools Develop your analytical abilities with
-                in-depth techniques in molecular simulations and enzyme studies.
-              </li>
-              <li>
-                Modeling for Real-World Applications Explore complex modeling
-                methods to tackle real-world bioinformatics challenges.
-              </li>
-              <li>
-                AI and Automation Master AI and machine learning applications
-                for automation, giving you a tech-driven edge in bioinformatics.
-              </li>
-            </ul>
-          </div>
+          {content.map((section, index) => (
+            <section key={index} className='content-section mb-8'>
+              <div className='flex flex-row justify-between items-start'>
+                <div className='flex-1 self-end text-gray-800'>
+                  <h2 className='md:text-3xl text-2xl font-semibold'>
+                    {section.title}
+                  </h2>
+                  <h2 className='md:text-4xl text-xl font-semibold'>
+                    {section.paragraphPrice}
+                  </h2>
+                  {section.type === "text" && (
+                    <>
+                      <p className='md:text-xl text-lg mb-6'>
+                        {section.paragraph}
+                      </p>
+                      <hr className='h-1 w-full bg-royalGreen rounded-lg' />
+                    </>
+                  )}
+                  {section.type === "list" && (
+                    <>
+                      <ul className='list-disc pl-6 mb-6'>
+                        {section.listItems.map((item, liIndex) => (
+                          <li className='md:text-xl text-lg' key={liIndex}>
+                            {item}
+                          </li>
+                        ))}
+                      </ul>
+                      <hr className='h-1 w-full bg-royalGreen rounded-lg' />
+                    </>
+                  )}
+                </div>
+                {(section.quantumImage || section.zummitImage) && (
+                  <div className='pl-4 md:block hidden'>
+                    <figure className='logo-container flex flex-col gap-2'>
+                      {section.zummitImage && (
+                        <img
+                          src={section.zummitImage}
+                          alt={section.zummitAlt}
+                          className='logo-image w-full h-auto max-w-32'
+                        />
+                      )}
+                      <figcaption className='text-center md:text-xl text-lg font-semibold text-gray-800'>
+                        In <strong>partnership</strong>
+                      </figcaption>
+                      {section.quantumImage && (
+                        <img
+                          src={section.quantumImage}
+                          alt={section.quantumAlt}
+                          className='logo-image w-full h-auto max-w-32'
+                        />
+                      )}
+                    </figure>
+                  </div>
+                )}
+              </div>
+            </section>
+          ))}
         </div>
       </div>
-    </article>
+    </section>
   );
 };
-
 export default LearnMore;
--- src/components/program/Program.jsx
+++ src/components/program/Program.jsx
@@ -3,26 +3,51 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import "./program.css";
 import DotsImage from "../assets/images/programImages/program-dots.png";
-
 gsap.registerPlugin(ScrollTrigger);
-
 const Program = () => {
   const bulbsRef = useRef([]);
-
   useEffect(() => {
+    const circles = bulbsRef.current.map((el) =>
+      el.querySelector(".outer-circle")
+    );
+    const numbers = bulbsRef.current.map((el) =>
+      el.querySelector(".program-steps")
+    );
     gsap.fromTo(
-      bulbsRef.current,
-      { x: window.innerWidth > 768 ? 150 : 50, autoAlpha: 0 }, // Start with reduced x offset on smaller screens
+      circles,
+      { backgroundColor: "#F8F8F8" }, // Initial background color
       {
-        x: 0,
-        autoAlpha: 1,
+        backgroundColor: "#003C2C", // Fill with royalGreen
         duration: 1,
         ease: "power2.out",
-        stagger: 0.2,
+        stagger: {
+          each: 1, // Sequential delay between fills
+          from: "start", // Fill circles from left to right
+        },
         scrollTrigger: {
-          trigger: bulbsRef.current,
+          trigger: bulbsRef.current[0],
           start: "top 80%",
           toggleActions: "play none none none",
+        },
+        onUpdate: function () {
+          numbers.forEach((num, i) => {
+            const circleBg = window.getComputedStyle(
+              circles[i]
+            ).backgroundColor;
+            if (circleBg === "rgb(0, 60, 44)") {
+              gsap.to(num, {
+                color: "#F8F8F8",
+                duration: 0.4,
+                overwrite: "auto",
+              });
+            }
+          });
+        },
+        onComplete: function () {
+          // Ensure all numbers, especially the last one, are white at the end.
+          numbers.forEach((num) => {
+            gsap.to(num, { color: "white", duration: 0.2 });
+          });
         },
       }
     );
@@ -54,7 +79,6 @@ const Program = () => {
       paragraphBot: "Optimizing processes",
     },
   ];
-
   return (
     <section className='program-section md:py-0 py-20 min-h-dvh flex flex-col justify-center overflow-hidden'>
       <div className='wrapper'>
@@ -68,18 +92,13 @@ const Program = () => {
               className='flex flex-col items-center lg:gap-10 gap-6 lg:max-w-fit'
               ref={(el) => (bulbsRef.current[index] = el)}
             >
-              <div className='program-dots relative h-full w-full mb-10'>
-                <h3 className='program-steps w-fit relative font-bold text-center md:text-8xl text-5xl rounded-full'>
+              <div className='circle-container relative w-[170px] h-[170px] flex items-center justify-center'>
+                <div className='outer-circle absolute rounded-full w-full h-full'></div>
+                <h3 className='program-steps relative z-10 text-black font-bold text-center md:text-8xl text-5xl'>
                   {programText.id}
                 </h3>
-                {index < programTexts.length - 1 && (
-                  <img
-                    className='absolute mediumLarge:block hidden top-[50%] -translate-y-[50%] -z-[3] left-[90%]'
-                    src={DotsImage}
-                    alt='dots'
-                  />
-                )}
               </div>
+
               <h3 className='font-bold md:text-2xl text-xl md:max-w-[200px] max-w-full'>
                 {programText.title}
               </h3>
